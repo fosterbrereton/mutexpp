3dc2a8238335bf1a2f3836a0ddb5d84e271f105e
Foster Brereton <fbrereto@adobe.com>
2016-12-21 13:14:36 -0800

    adding debug stuff

--- include/spin_adaptor.hpp
+++ include/spin_adaptor.hpp
@@ -14,32 +14,63 @@
 
 /******************************************************************************/
 
+#ifndef qDebug
+#define qDebug 1
+#endif
+
+/******************************************************************************/
+
 template <typename Lockable>
 class spin_adaptor
 {
     using clock_t = std::chrono::high_resolution_clock;
     using time_point_t = clock_t::time_point;
+
+public:
     using rep_t = decltype((std::declval<time_point_t>() - std::declval<time_point_t>()).count());
 
+private:
     std::atomic<rep_t> expected_m{1};
     Lockable           mutex_m;
 
 public:
+
+#if qDebug
+    void(*probe_m)(bool did_block, rep_t new_expected);
+#endif
+
     void lock() {
         thread_local time_point_t  before = clock_t::now();
         thread_local rep_t         elapsed{0};
 
+#if qDebug
+        bool                       did_block{false};
+#endif
+
+        const rep_t time_to_block = expected_m * 2;
+
         while (!mutex_m.try_lock()) {
             elapsed = (clock_t::now() - before).count();
 
-            if (elapsed > (expected_m * 2)) {
+            if (elapsed >= time_to_block) {
                 mutex_m.lock();
+
+                did_block = true;
 
                 break;
             }
         }
 
+        // The larger the divisor, the less effect the current
+        // elapsed count will have on the expected value.
+        // So, assuming a perfectly stable area of contention,
+        // 4 would stabilize faster than 8.
         expected_m += (elapsed - expected_m) / 8;
+
+#if qDebug
+        if (probe_m)
+            probe_m(did_block, expected_m);
+#endif
     }
 
     void unlock() {
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <mutex>
 #include <thread>
 #include <vector>
+#include <fstream>
 
 // spin_adaptor
 #include "spin_adaptor.hpp"
@@ -39,24 +40,61 @@ private:
 
 /******************************************************************************/
 
+struct thread_clock_t {
+    void reset() {
+        clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start_m);
+    }
+
+    double split() const {
+        timespec t;
+
+        clock_gettime(CLOCK_THREAD_CPUTIME_ID, &t);
+
+        return (t.tv_sec - start_m.tv_sec) * 1000. + ((t.tv_nsec - start_m.tv_nsec) / 1000000.);
+    }
+
+private:
+    timespec start_m;
+};
+
+/******************************************************************************/
+
 using clk_t = std::chrono::high_resolution_clock;
 using spin_t = spin_adaptor<std::mutex>;
 using spinlock_t = std::lock_guard<spin_t>;
 
+/******************************************************************************/
+
+void probe(bool did_block, spin_t::rep_t new_expected) {
+    static std::size_t   count_s{0};
+    static std::size_t   blocked_s{0};
+    static std::ofstream output("outfile.csv");
+
+    ++count_s;
+
+    if (did_block)
+        ++blocked_s;
+
+    output << new_expected
+           << ',' << static_cast<int>(did_block)
+           << ',' << static_cast<double>(blocked_s) / count_s
+           << '\n';
+}
+
 template <typename Clock>
 void worker(spin_t& mutex) {
-    constexpr long milliseconds_k{1000000};
+    constexpr long ms_per_ns_k{1000000};
 
-    Clock    clock;
+    //Clock    clock;
     timespec sleep_amount = {
-        0, 1 * milliseconds_k 
+        0, 1 * ms_per_ns_k
     };
-    for (std::size_t i(0); i < 10; ++i) {
-        clock.reset();
+
+    for (std::size_t i(0); i < 100; ++i) {
+        //clock.reset();
         spinlock_t lock(mutex);
-        auto time = clock.split();
-        std::cout << time << "\n";
-        nanosleep(&sleep_amount, nullptr);
+        //auto time = clock.split();
+        //nanosleep(&sleep_amount, nullptr);
     }
 }
 
@@ -64,14 +102,10 @@ int main(int argc, char** argv) {
     std::vector<std::thread> pool;
     spin_t                   mutex;
 
-#if 1
-    using clock_type = wall_clock_t;
-#else
-    using clock_type = cpu_clock_t;
-#endif
+    mutex.probe_m = &probe;
 
     for (std::size_t i(0); i < 10; ++i) {
-        pool.emplace_back(std::bind(worker<clock_type>, std::ref(mutex)));
+        pool.emplace_back(std::bind(worker<wall_clock_t>, std::ref(mutex)));
     }
 
     for (auto& t : pool) {
